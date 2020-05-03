6263396d89196305d778bcc877a9e20dbabe3f60
Matthew H. Irby <matt.irby@outlook.com>
2020-05-03 18:17:40 -0400

    End of section 7

---
+++ DatingApp-SPA/src/app/member-list/member-list.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-member-lists',
+  templateUrl: './member-list.component.html',
+  styleUrls: ['./member-list.component.css'],
+})
+export class MemberListComponent {}
